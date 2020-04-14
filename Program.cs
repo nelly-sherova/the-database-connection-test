732a29cdbc4244f160ce5b95c3812f9c66ede52c
unknown <lvova.nikol@mail.ru>
2020-04-14 17:24:23 +0300

    Project1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Connecting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
