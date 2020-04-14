a5d0da02ac99e483e2f9bf4d681d559e40012657
unknown <lvova.nikol@mail.ru>
2020-04-14 17:52:49 +0300

    Соединение б/з с CSharp

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace Connecting
+namespace MyConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const string conString = @"Data source=NILUFARSHEROVA; Initial catalog=AlifAcademy; Integrated Security = True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            if (con.State == ConnectionState.Open)System.Console.WriteLine("Connected successfully");
+            else System.Console.WriteLine("Troubles with connection"); 
+            string commandText  = "Select *from TablePerson";
+            SqlCommand command = new SqlCommand(commandText, con);
+            SqlDataReader reader  = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"ID: {reader.GetValue(0)}, Firstname:{reader.GetValue(1)}, LastName:{reader.GetValue(2)}");
+            }
         }
     }
 }
